a5d2db4a0a9a6ff4bb02fc967e877ae6a79c7772
nurzafri <nurzafri@gmail.com>
2017-03-18 22:52:54 +0800

    can add employee

--- app/src/main/java/com/example/tutorial/materialdesign/AnnouncementFragment.java
+++ app/src/main/java/com/example/tutorial/materialdesign/AnnouncementFragment.java
@@ -1,11 +1,22 @@
 package com.example.tutorial.materialdesign;
 
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.tutorial.materialdesign.Config.Config;
+import com.example.tutorial.materialdesign.RequestHandler.RequestHandler;
+
+import java.util.HashMap;
 
 
 /**
@@ -13,6 +24,13 @@ import android.view.ViewGroup;
  */
 public class AnnouncementFragment extends android.support.v4.app.Fragment {
 
+    //Defining views
+    private EditText editTextName;
+    private EditText editTextDesg;
+    private EditText editTextSal;
+
+    private Button buttonAdd;
+//    private Button buttonView;
 
     public AnnouncementFragment() {
         // Required empty public constructor
@@ -23,7 +41,64 @@ public class AnnouncementFragment extends android.support.v4.app.Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.announcement_fragment, container, false);
+        View v = inflater.inflate(R.layout.announcement_fragment, container, false);
+        //Initializing views
+        editTextName = (EditText) v.findViewById(R.id.editTextName);
+        editTextDesg = (EditText) v.findViewById(R.id.editTextDesg);
+        editTextSal = (EditText) v.findViewById(R.id.editTextSalary);
+
+        buttonAdd = (Button) v.findViewById(R.id.buttonAdd);
+
+        //Setting listeners to button
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    addEmployee();
+                }
+        });
+        return v;
     }
+
+    //Adding an employee
+    private void addEmployee(){
+
+        final String name = editTextName.getText().toString().trim();
+        final String desg = editTextDesg.getText().toString().trim();
+        final String sal = editTextSal.getText().toString().trim();
+
+        class AddEmployee extends AsyncTask<Void,Void,String> {
+
+            ProgressDialog loading;
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loading = ProgressDialog.show(getActivity(),"Adding...","Wait...",false,false);
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                loading.dismiss();
+                Toast.makeText(getActivity(),s,Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            protected String doInBackground(Void... v) {
+                HashMap<String,String> params = new HashMap<>();
+                params.put(Config.KEY_EMP_NAME,name);
+                params.put(Config.KEY_EMP_DESG,desg);
+                params.put(Config.KEY_EMP_SAL,sal);
+
+                RequestHandler rh = new RequestHandler();
+                String res = rh.sendPostRequest(Config.URL_ADD, params);
+                return res;
+            }
+        }
+
+        AddEmployee ae = new AddEmployee();
+        ae.execute();
+    }
+
 
 }
